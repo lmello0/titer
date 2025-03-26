52d33cafa108ce87bece125a2bd920e50f23a17a
Lucas Mello dos Santos <contact@lmello.dev>
2025-03-26 01:07:02 -0300

    :sparkles: feat: add lists (galleries) to home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { TrendingComponent } from './trending/trending.component';
+import { NewPlaysComponent } from './new-plays/new-plays.component';
+import { MostPopularAllTimeComponent } from './most-popular-all-time/most-popular-all-time.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TrendingComponent],
+  imports: [TrendingComponent, NewPlaysComponent, MostPopularAllTimeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

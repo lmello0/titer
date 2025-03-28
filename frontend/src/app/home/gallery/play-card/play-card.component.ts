36d83ae3f57b9238f18b11554e5d17943bc2aa4a
Lucas Mello dos Santos <contact@lmello.dev>
2025-03-27 23:46:16 -0300

    :lipstick: feat: finish restyle of play-card

--- frontend/src/app/home/gallery/play-card/play-card.component.ts
+++ frontend/src/app/home/gallery/play-card/play-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-play-card',
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './play-card.component.html',
   styleUrl: './play-card.component.css',
 })
-export class PlayCardComponent {
-  reviews: number = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-  watched: number = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+export class PlayCardComponent implements OnInit {
+  @Input() reviews!: string;
+  @Input() watched!: string;
+  @Input() url: string =
+    'https://m.media-amazon.com/images/I/81kz06oSUeL._AC_SL1500_.jpg';
+
+  ngOnInit(): void {
+    const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+
+    this.reviews = formatter.format(
+      Math.floor(Math.random() * (1_000_000 * 10)),
+    );
+
+    this.watched = formatter.format(
+      Math.floor(Math.random() * (1_000_000 * 10)),
+    );
+  }
 }

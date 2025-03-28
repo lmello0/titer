ebd884d91d25976d1defe45eebc42f8ecbba96af
Lucas Mello dos Santos <contact@lmello.dev>
2025-03-28 07:38:07 -0300

    :construction: feat: idk what this commit is about yet

--- frontend/src/app/home/gallery/gallery.component.ts
+++ frontend/src/app/home/gallery/gallery.component.ts
@@ -1,14 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PlayCardComponent } from './play-card/play-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  imports: [PlayCardComponent],
+  imports: [CommonModule, PlayCardComponent],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css',
 })
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
   @Input() title: string = '';
 
-  items = [1, 2, 3, 4, 5, 6];
+  items: any[] = [];
+
+  ngOnInit(): void {
+    const urls: string[] = [
+      'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg',
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
+      'https://image.tmdb.org/t/p/w600_and_h900_bestv2/sF1U4EUQS8YHUYjNl3pMGNIQyr0.jpg',
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
+      'https://image.tmdb.org/t/p/w600_and_h900_bestv2/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg',
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg',
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8VG8fDNiy50H4FedGwdSVUPoaJe.jpg',
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vQWk5YBFWF4bZaofAbv0tShwBvQ.jpg',
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/q719jXXEzOoYaps6babgKnONONX.jpg',
+      'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg',
+    ];
+
+    for (let i = 0; i < 5; i++) {
+      const index = Math.floor(Math.random() * urls.length);
+      const url = urls[index];
+
+      urls.splice(index, 1);
+
+      this.items.push({
+        reviewCount: Math.floor(Math.random() * (1_000_000 * 10)),
+        watchedCount: Math.floor(Math.random() * (1_000_000 * 10)),
+        imageSrc: url,
+      });
+    }
+  }
 }
--- frontend/src/app/home/gallery/play-card/play-card.component.ts
+++ frontend/src/app/home/gallery/play-card/play-card.component.ts
@@ -7,20 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './play-card.component.css',
 })
 export class PlayCardComponent implements OnInit {
-  @Input() reviews!: string;
-  @Input() watched!: string;
-  @Input() url: string =
-    'https://m.media-amazon.com/images/I/81kz06oSUeL._AC_SL1500_.jpg';
+  @Input() reviews!: number | string;
+  @Input() watched!: number | string;
+  @Input() url!: string;
 
   ngOnInit(): void {
     const formatter = Intl.NumberFormat('en', { notation: 'compact' });
 
-    this.reviews = formatter.format(
-      Math.floor(Math.random() * (1_000_000 * 10)),
-    );
+    this.reviews = formatter.format(this.reviews as number);
 
-    this.watched = formatter.format(
-      Math.floor(Math.random() * (1_000_000 * 10)),
-    );
+    this.watched = formatter.format(this.watched as number);
   }
 }

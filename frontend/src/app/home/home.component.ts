87cc77eaa520baa725e2b087687d1d1bdd66ccfa
Lucas Mello dos Santos <contact@lmello.dev>
2025-03-31 13:38:48 -0300

    :recycle: refactor: making navbar transparent

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { TrendingComponent } from './trending/trending.component';
 import { NewPlaysComponent } from './new-plays/new-plays.component';
 import { MostPopularAllTimeComponent } from './most-popular-all-time/most-popular-all-time.component';
+import { SpotlightComponent } from './spotlight/spotlight.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TrendingComponent, NewPlaysComponent, MostPopularAllTimeComponent],
+  imports: [
+    TrendingComponent,
+    NewPlaysComponent,
+    MostPopularAllTimeComponent,
+    SpotlightComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

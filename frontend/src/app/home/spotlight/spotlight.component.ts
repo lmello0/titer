c23f32ed187b765b9c8464b1e7c1d1f8b932197d
Lucas Mello dos Santos <contact@lmello.dev>
2025-03-31 15:04:07 -0300

    :sparkles: feat: add spotlight at home

---
+++ frontend/src/app/home/spotlight/spotlight.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spotlight',
+  imports: [],
+  templateUrl: './spotlight.component.html',
+  styleUrl: './spotlight.component.css'
+})
+export class SpotlightComponent {
+
+}

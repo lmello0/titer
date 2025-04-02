b9d95233927ec550e810443e7f1c8d8508c351cc
Lucas Mello dos Santos <contact@lmello.dev>
2025-04-02 00:11:10 -0300

    :sparkles: feat: add spotlight

--- frontend/src/app/home/spotlight/spotlight.component.ts
+++ frontend/src/app/home/spotlight/spotlight.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spotlight',
   imports: [],
   templateUrl: './spotlight.component.html',
-  styleUrl: './spotlight.component.css'
+  styleUrl: './spotlight.component.css',
 })
-export class SpotlightComponent {
+export class SpotlightComponent implements OnInit {
+  urls: string[] = [
+    'https://film-grab.com/wp-content/uploads/2024/07/Robin-Hood-73-19.jpg',
+    'https://film-grab.com/wp-content/uploads/2024/07/The-Lion-King-64.jpg',
+    'https://film-grab.com/wp-content/uploads/2024/07/ATGIB-38.jpg',
+    'https://film-grab.com/wp-content/uploads/2024/05/A-Matter-of-Life-and-Death-62.jpg',
+    'https://film-grab.com/wp-content/uploads/2012/11/29.jpg',
+  ];
 
+  imageUrl: string = '';
+
+  ngOnInit() {
+    this.imageUrl = this.urls[Math.floor(Math.random() * this.urls.length)];
+  }
 }

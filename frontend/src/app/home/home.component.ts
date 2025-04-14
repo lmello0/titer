e57581928cd124dfe144a8c7d73d23c93e8338d7
Lucas Mello dos Santos <contact@lmello.dev>
2025-04-14 01:16:34 -0300

    :sparkles: feat: adding footer

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { TrendingComponent } from './trending/trending.component';
 import { SpotlightComponent } from './spotlight/spotlight.component';
 import { FeaturesComponent } from './features/features.component';
 import { LastReviewsComponent } from './last-reviews/last-reviews.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import { LastReviewsComponent } from './last-reviews/last-reviews.component';
     SpotlightComponent,
     FeaturesComponent,
     LastReviewsComponent,
+    FooterComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',

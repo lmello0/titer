886cb7f6f7625b3c8b6a1f0ebe6d0fe52e5e06e9
Lucas Mello dos Santos <contact@lmello.dev>
2025-05-12 15:40:09 -0300

    :bug: fix: fix a bug where allow scrolling when navbar is open on mobile

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NgClass } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { MobileNavbarOpenService } from '../services/menu-open/mobile-navbar-open.service';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, NgClass, RouterOutlet, FooterComponent],
+  imports: [NavbarComponent, CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,7 +3,6 @@ import { TrendingComponent } from './trending/trending.component';
 import { SpotlightComponent } from './spotlight/spotlight.component';
 import { FeaturesComponent } from './features/features.component';
 import { LastReviewsComponent } from './last-reviews/last-reviews.component';
-import { FooterComponent } from '../footer/footer.component';
 import { NgClass } from '@angular/common';
 import { MobileNavbarOpenService } from '../../services/menu-open/mobile-navbar-open.service';
 
@@ -14,8 +13,11 @@ import { MobileNavbarOpenService } from '../../services/menu-open/mobile-navbar-
     SpotlightComponent,
     FeaturesComponent,
     LastReviewsComponent,
+    NgClass,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(public isMenuOpenService: MobileNavbarOpenService) {}
+}

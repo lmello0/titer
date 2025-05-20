688d30b144b7e945b51dca2a3e98766b32607d8d
Lucas Mello dos Santos <contact@lmello.dev>
2025-05-20 00:20:49 -0300

    :sparkles: feat: add "add your review" feature

--- frontend/src/app/play-page/comment-section/comment-section.component.ts
+++ frontend/src/app/play-page/comment-section/comment-section.component.ts
@@ -10,9 +10,38 @@ import { CommonModule } from '@angular/common';
 })
 export class CommentSectionComponent {
   @Input() comments!: Comment[];
+  @Input() rating = 0;
+
+  hoverValue: number | null = null;
 
   toggleLike(comment: Comment): void {
     comment.likedByUser = !comment.likedByUser;
     comment.likes += comment.likedByUser ? 1 : -1;
+  }
+
+  autoResize(event: Event): void {
+    const textarea = event.target as HTMLTextAreaElement;
+
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+  }
+
+  setRating(newRating: number) {
+    this.rating = newRating;
+  }
+
+  setHover(value: number | null) {
+    this.hoverValue = value;
+  }
+
+  getFillPercentage(starIndex: number): number {
+    const value = this.hoverValue ?? this.rating;
+    const diff = value - starIndex;
+
+    if (diff >= 1) return 100;
+    if (diff >= 0.5) return 50;
+    if (diff > 0) return diff * 100;
+
+    return 0;
   }
 }

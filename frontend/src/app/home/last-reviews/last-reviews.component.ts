29dc2d0c812ef92535965cb3717598920cfeb339
Lucas Mello dos Santos <contact@lmello.dev>
2025-05-20 23:19:59 -0300

    :recycle: refactor: update lastReviews structure to use playId and author fields, replace reviewText with content

--- frontend/src/app/home/last-reviews/last-reviews.component.ts
+++ frontend/src/app/home/last-reviews/last-reviews.component.ts
@@ -12,154 +12,94 @@ import { RouterLink } from '@angular/router';
 export class LastReviewsComponent {
   lastReviews: Review[] = [
     {
-      play: {
-        title: 'The Shawshank Redemption',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg',
-        date: {
-          fullDate: new Date(),
-          day: 1,
-          month: 1,
-          year:
-            Math.floor(Math.random() * (new Date().getFullYear() - 1930 + 1)) +
-            1930,
-        },
+      id: 1,
+      playId: Math.floor(Math.random() * 10),
+      author: {
+        userId: 1,
+        username: 'John Doe',
+        profilePhotoUrl: 'https://i.pravatar.cc/150?img=5',
       },
-      user: {
-        name: 'User',
-        avatarUrl:
-          'https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png',
-      },
-      reviewText:
+      content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt, odio, incidunt nulla quidem repellendus labore, maiores velit delectus earum numquam! Assumenda fuga voluptates expedita accusamus amet placeat veritatis voluptate.',
+      date: new Date(),
+      likedByUser: false,
+      likes: Math.floor(Math.random() * 1_000),
       rating: 0.5 + Math.floor(Math.random() * 10) * 0.5,
-      numLikes: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
-      numComments: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
     },
     {
-      play: {
-        title: 'The Shawshank Redemption',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg',
-        date: {
-          fullDate: new Date(),
-          day: 1,
-          month: 1,
-          year:
-            Math.floor(Math.random() * (new Date().getFullYear() - 1930 + 1)) +
-            1930,
-        },
+      id: 1,
+      playId: Math.floor(Math.random() * 10),
+      author: {
+        userId: 1,
+        username: 'John Doe',
+        profilePhotoUrl: 'https://i.pravatar.cc/150?img=5',
       },
-      user: {
-        name: 'User',
-        avatarUrl:
-          'https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png',
-      },
-      reviewText:
+      content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt, odio, incidunt nulla quidem repellendus labore, maiores velit delectus earum numquam! Assumenda fuga voluptates expedita accusamus amet placeat veritatis voluptate.',
+      date: new Date(),
+      likedByUser: false,
+      likes: Math.floor(Math.random() * 1_000),
       rating: 0.5 + Math.floor(Math.random() * 10) * 0.5,
-      numLikes: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
-      numComments: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
     },
     {
-      play: {
-        title: 'The Shawshank Redemption',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg',
-        date: {
-          fullDate: new Date(),
-          day: 1,
-          month: 1,
-          year:
-            Math.floor(Math.random() * (new Date().getFullYear() - 1930 + 1)) +
-            1930,
-        },
+      id: 1,
+      playId: Math.floor(Math.random() * 10),
+      author: {
+        userId: 1,
+        username: 'John Doe',
+        profilePhotoUrl: 'https://i.pravatar.cc/150?img=5',
       },
-      user: {
-        name: 'User',
-        avatarUrl:
-          'https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png',
-      },
-      reviewText:
+      content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt, odio, incidunt nulla quidem repellendus labore, maiores velit delectus earum numquam! Assumenda fuga voluptates expedita accusamus amet placeat veritatis voluptate.',
+      date: new Date(),
+      likedByUser: false,
+      likes: Math.floor(Math.random() * 1_000),
       rating: 0.5 + Math.floor(Math.random() * 10) * 0.5,
-      numLikes: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
-      numComments: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
     },
     {
-      play: {
-        title: 'The Shawshank Redemption',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg',
-        date: {
-          fullDate: new Date(),
-          day: 1,
-          month: 1,
-          year:
-            Math.floor(Math.random() * (new Date().getFullYear() - 1930 + 1)) +
-            1930,
-        },
+      id: 1,
+      playId: Math.floor(Math.random() * 10),
+      author: {
+        userId: 1,
+        username: 'John Doe',
+        profilePhotoUrl: 'https://i.pravatar.cc/150?img=5',
       },
-      user: {
-        name: 'User',
-        avatarUrl:
-          'https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png',
-      },
-      reviewText:
+      content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt, odio, incidunt nulla quidem repellendus labore, maiores velit delectus earum numquam! Assumenda fuga voluptates expedita accusamus amet placeat veritatis voluptate.',
+      date: new Date(),
+      likedByUser: false,
+      likes: Math.floor(Math.random() * 1_000),
       rating: 0.5 + Math.floor(Math.random() * 10) * 0.5,
-      numLikes: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
-      numComments: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
     },
     {
-      play: {
-        title: 'The Shawshank Redemption',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg',
-        date: {
-          fullDate: new Date(),
-          day: 1,
-          month: 1,
-          year:
-            Math.floor(Math.random() * (new Date().getFullYear() - 1930 + 1)) +
-            1930,
-        },
+      id: 1,
+      playId: Math.floor(Math.random() * 10),
+      author: {
+        userId: 1,
+        username: 'John Doe',
+        profilePhotoUrl: 'https://i.pravatar.cc/150?img=5',
       },
-      user: {
-        name: 'User',
-        avatarUrl:
-          'https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png',
-      },
-      reviewText:
+      content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt, odio, incidunt nulla quidem repellendus labore, maiores velit delectus earum numquam! Assumenda fuga voluptates expedita accusamus amet placeat veritatis voluptate.',
+      date: new Date(),
+      likedByUser: false,
+      likes: Math.floor(Math.random() * 1_000),
       rating: 0.5 + Math.floor(Math.random() * 10) * 0.5,
-      numLikes: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
-      numComments: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
     },
     {
-      play: {
-        title: 'The Shawshank Redemption',
-        posterUrl:
-          'https://image.tmdb.org/t/p/w600_and_h900_bestv2/9cqNxx0GxF0bflZmeSMuL5tnGzr.jpg',
-        date: {
-          fullDate: new Date(),
-          day: 1,
-          month: 1,
-          year:
-            Math.floor(Math.random() * (new Date().getFullYear() - 1930 + 1)) +
-            1930,
-        },
+      id: 1,
+      playId: Math.floor(Math.random() * 10),
+      author: {
+        userId: 1,
+        username: 'John Doe',
+        profilePhotoUrl: 'https://i.pravatar.cc/150?img=5',
       },
-      user: {
-        name: 'User',
-        avatarUrl:
-          'https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png',
-      },
-      reviewText:
+      content:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt, odio, incidunt nulla quidem repellendus labore, maiores velit delectus earum numquam! Assumenda fuga voluptates expedita accusamus amet placeat veritatis voluptate.',
+      date: new Date(),
+      likedByUser: false,
+      likes: Math.floor(Math.random() * 1_000),
       rating: 0.5 + Math.floor(Math.random() * 10) * 0.5,
-      numLikes: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
-      numComments: Math.floor(Math.random() * (10_000 - 1_00 + 1)) + 1_00,
     },
   ];
 }

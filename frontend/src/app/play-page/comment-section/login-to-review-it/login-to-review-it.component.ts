7a99c5105670e68395fe6a668895a895de7b534a
Lucas Mello dos Santos <contact@lmello.dev>
2025-05-27 19:23:44 -0300

    sparkles: feat: add call to sign-in component on click

--- frontend/src/app/play-page/comment-section/login-to-review-it/login-to-review-it.component.ts
+++ frontend/src/app/play-page/comment-section/login-to-review-it/login-to-review-it.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SignInComponent } from '../../../sign-in/sign-in.component';
 
 @Component({
   selector: 'app-login-to-review-it',
-  imports: [RouterLink],
+  imports: [RouterLink, SignInComponent],
   templateUrl: './login-to-review-it.component.html',
   styleUrl: './login-to-review-it.component.css',
 })
-export class LoginToReviewItComponent {}
+export class LoginToReviewItComponent {
+  isLoginOpen = false;
+}

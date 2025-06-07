a2c2038b73587ee449251d9c8c0a0a881c5e09d9
Lucas Mello dos Santos <contact@lmello.dev>
2025-06-07 00:18:50 -0300

    :sparkles: feat: finishing selectable-list input

--- frontend/src/app/shared/components/selectable-list/selectable-list.component.ts
+++ frontend/src/app/shared/components/selectable-list/selectable-list.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Option } from '../search-input/interface/option.interface';
+import { debounce, debounceTime, distinctUntilChanged, startWith, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-selectable-list',
@@ -9,61 +10,119 @@ import { Option } from '../search-input/interface/option.interface';
   templateUrl: './selectable-list.component.html',
   styleUrl: './selectable-list.component.css',
 })
-export class SelectableListComponent {
+export class SelectableListComponent implements OnInit, OnChanges, OnDestroy {
   @Input() options: Option[] = [];
   @Input() allowAddNew: boolean = true;
   @Input() addNewLabel: string = 'Add new option';
   @Input() placeholder: string = 'Search';
+  @Input() newLabel = 'As:';
+  @Input() newPlaceholder = '';
 
   @Output() selectedChange = new EventEmitter<Option[]>();
 
   selected: Option[] = [];
+  filteredOptions: Option[] = [];
+
   showDropdown = false;
-  formControl = new FormControl('');
+  showNewField = false;
 
-  constructor() {}
+  searchControl = new FormControl('');
+  newOptionControl = new FormControl('');
 
-  onFocus() {
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.filteredOptions = [...this.options];
+
+    this.searchControl.valueChanges
+      .pipe(startWith(''), debounceTime(300), distinctUntilChanged(), takeUntil(this.destroy$))
+      .subscribe((searchTerm) => this.filterOptions(searchTerm ?? ''));
+  }
+
+  ngOnChanges() {
+    // this.filterOptions(this.searchControl.value ?? '');
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onFocus(): void {
     this.showDropdown = true;
+    this.filterOptions(this.searchControl.value ?? '');
   }
 
-  onBlur() {
-    setTimeout(() => {
-      this.showDropdown = false;
-    }, 200);
+  onBlur(): void {
+    setTimeout(() => (this.showDropdown = false), 200);
   }
 
-  selectOption(option: Option): void {
-    this.formControl.setValue(option.label);
+  selectOption(option?: Option): void {
+    const isAddNew = !option;
+
+    this.searchControl.setValue(isAddNew ? this.addNewLabel : option!.label, { emitEvent: false });
+    this.showNewField = isAddNew;
     this.showDropdown = false;
   }
 
   onAddItem() {
-    const item = this.options.find((o) => o.label === this.formControl.value);
+    const label = this.searchControl.value ?? '';
+    const isNew = this.searchControl.value === this.addNewLabel;
 
-    if (item) {
-      this.selected.push(item);
+    const newItem = isNew ? this.createNewItem() : this.extractOption(label);
 
-      this.options.splice(
-        this.options.findIndex((o) => o.id === item.id),
-        1,
-      );
+    if (!newItem || this.isAlreadySelected(newItem)) return;
 
-      this.formControl.reset();
-    }
-
-    this.selectedChange.emit(this.selected);
+    this.selected.push(newItem);
+    this.resetInputs();
+    this.emitSelected();
   }
 
   onRemoveItem(item: Option) {
-    const itemIndex = this.selected.findIndex((i) => i === item);
+    this.selected = this.selected.filter((i) => i !== item);
 
-    if (itemIndex > -1) {
-      this.selected.splice(itemIndex, 1);
-      this.options.push(item);
-      this.options.sort((a, b) => parseInt(a.id.toString()) - parseInt(b.id.toString()));
+    if (item.id !== -1) {
+      this.options = [...this.options, item].sort((a, b) => Number(a.id) - Number(b.id));
     }
 
-    this.selectedChange.emit(this.selected);
+    this.filteredOptions = [...this.options];
+    this.emitSelected();
+  }
+
+  private createNewItem(): Option {
+    return { id: -1, label: this.newOptionControl.value ?? '', value: -1 };
+  }
+
+  private extractOption(label: string): Option | undefined {
+    const match = this.options.find((o) => o.label === label);
+
+    if (!match) return undefined;
+
+    this.options = this.options.filter((o) => o !== match);
+    return match;
+  }
+
+  private isAlreadySelected(option: Option): boolean {
+    return this.selected.some((sel) => sel.label === option.label);
+  }
+
+  private resetInputs(): void {
+    this.searchControl.reset();
+    this.newOptionControl.reset();
+    this.showNewField = false;
+  }
+
+  private emitSelected(): void {
+    this.selectedChange.emit([...this.selected]);
+  }
+
+  private filterOptions(searchTerm: string) {
+    if (!searchTerm.trim()) {
+      this.filteredOptions = [...this.options];
+      return;
+    }
+
+    const lower = searchTerm.toLowerCase();
+    this.filteredOptions = this.options.filter((o) => o.label.toLowerCase().startsWith(lower));
   }
 }

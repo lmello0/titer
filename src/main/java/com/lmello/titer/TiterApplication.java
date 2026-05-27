1b64ca7a95a99c6d7cce6aa9d056fce64303797d
Lucas Mello dos Santos <contact@lmello.dev>
2026-05-26 22:34:38 -0300

    :tada: init: initial commit

---
+++ src/main/java/com/lmello/titer/TiterApplication.java
@@ -0,0 +1,11 @@
+package com.lmello.titer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiterApplication {
+	static void main(String[] args) {
+		SpringApplication.run(TiterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lmello/titer/TiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmello.titer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

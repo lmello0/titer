9c3ab54b0749a52a0d89d56b99968b9540318750
Lucas Mello dos Santos <contact@lmello.dev>
2026-05-26 23:45:38 -0300

    :sparkles: feat: set up modular monolith arch tests

---
+++ src/test/java/com/lmello/titer/ModularityTest.java
@@ -0,0 +1,36 @@
+package com.lmello.titer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@ApplicationModuleTest
+public class ModularityTest {
+
+    ApplicationModules modules = ApplicationModules.of(TiterApplication.class);
+
+    @Test
+    void verifiesModuleStructure() {
+        modules.verify();
+    }
+
+    @Test
+    void rendersDiagram() {
+        new Documenter(modules)
+                .writeModulesAsPlantUml()
+                .writeIndividualModulesAsPlantUml();
+    }
+
+    @Test
+    void printModuleInfo() {
+        modules.forEach(System.out::println);
+    }
+
+    @Test
+    void noCircularDependencies() {
+        assertThatNoException().isThrownBy(modules::verify);
+    }
+}
